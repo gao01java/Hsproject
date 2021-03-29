b94a749e8a29a01740c71345ac0bbc0ce4937f51
gao01java <60835579+gao01java@users.noreply.github.com>
2021-03-29 19:47:09 +0900

    order_xml icon+

--- app/src/main/java/com/example/hsproject/OrderActivity.java
+++ app/src/main/java/com/example/hsproject/OrderActivity.java
@@ -1,15 +1,18 @@
 package com.example.hsproject;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.transition.Transition;
 import android.transition.TransitionInflater;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class OrderActivity extends AppCompatActivity {
 
-
+    ImageButton backB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +23,19 @@ public class OrderActivity extends AppCompatActivity {
             Transition explode = TransitionInflater.from(this).inflateTransition(R.transition.explode);
             getWindow().setEnterTransition(explode);
         }
+
+
+
+
+        backB=(ImageButton)findViewById(R.id.bt_back);
+        backB.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(OrderActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        }));
+
 
 
     }
